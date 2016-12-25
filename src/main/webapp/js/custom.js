abb3488a45d7a6f443f7cb82bf3ec8589505083b
Kunal Gautam <kunal@ikunal.in>
2016-12-25 18:47:20 +0530

    [mod] Added functionality to delete the ToDo list.

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -27,7 +27,7 @@ function checkAddData(data, statusText, xhr, task_value) {
   	
   	if (status == 201) {
   		var id = data["id"]; // Set ID retreived from response
-  		$(tasklist).append('<tr><td>'+task_value+'</td><td><a class="btn btn-info" href="#mod'+id+'" role="button">Modify</a></td><td><a class="btn btn-danger" href="#del'+id+'" role="button">Delete</a></td></tr>');//Append with value retrieve from text box to DOM of task list
+  		$(tasklist).append('<tr id="row_'+task_value+'"><td>'+task_value+'</td><td><a class="btn btn-info" href="#mod'+id+'" role="button">Modify</a></td><td><a class="btn btn-danger" id="todo_data_delete" href="#" value="'+id+'" role="button">Delete</a></td></tr>');//Append with value retrieve from text box to DOM of task list
   		$("#task_name").val('');//And after append clear the text box
   	}
   	else {
@@ -77,5 +77,33 @@ function renderTable(rowData){
 	var tasklist = $(".task_lists"); // Retreive DOM of task_list class
 	var task_value = rowData.text;
 	var id = rowData.id;
-	$(tasklist).append('<tr><td>'+task_value+'</td><td><a class="btn btn-info" href="#mod'+id+'" role="button">Modify</a></td><td><a class="btn btn-danger" href="#del'+id+'" role="button">Delete</a></td></tr>');//Append with value retrieve from get method
+	$(tasklist).append('<tr id="row_'+id+'"><td>'+task_value+'</td><td><a class="btn btn-info todo-data-modify" id="'+id+'" href="#mod'+id+'" role="button">Modify</a></td><td><a class="btn btn-danger" id="todo_data_delete" href="#" value="'+id+'" role="button">Delete</a></td></tr>');//Append with value retrieve from get method
 }
+
+// Delete Function, to be strange, $("#todo_data_delete").click(function(){}); was not working hence this method is used to call. Most probably due to it fact that these rows are ajax genereted.
+$(document).on("click", "#todo_data_delete", function() {
+	var value = $(this).attr("value");
+	var netalrt =  $(".network-alert");
+	
+	$.ajax({
+        url: './api/remove/'+value,
+        type: 'DELETE',
+		success: function(){
+			//Upon success in deleting, notify user
+			$(netalrt).html('<div class="alert alert-info" id="network-delete-alert"> <button type="button" class="close" data-dismiss="alert">x</button> <strong>Deleted! </strong> To-Do is deleted.</div>'); // replace the value to network alert DOM
+			$("#network-delete-alert").fadeTo(2000, 500).slideUp(500, function(){
+			    $("network-error-alert").alert('close');
+			});
+			// And then delete the table which contain data.
+			$("#row_" + value ).remove(); 
+		},
+		error: function() { 
+
+			$(netalrt).html('<div class="alert alert-danger" id="network-delete-fail-alert"> <button type="button" class="close" data-dismiss="alert">x</button> <strong>Oops! </strong> Unable to delete the To-Do. Please retry.</div>'); // replace the value to network alert DOM
+			$("#network-delete-fail-alert").fadeTo(2000, 500).slideUp(500, function(){
+			    $("network-error-alert").alert('close');
+			});
+		
+		}
+    });
+});
